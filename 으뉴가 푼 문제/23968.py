7a92eacdeddad652f6b02c785f4dbe831c4161c3
Eunyu <82995817+ChunEunyu@users.noreply.github.com>
2022-08-08 14:29:57 +0900

    Create 23968.py

---
+++ 으뉴가 푼 문제/23968.py
@@ -0,0 +1,25 @@
+import sys
+
+"""
+앞에서 부터 탐색.
+옆에 있는 두 수를 교환.
+
+"""
+def bubble_sort(arr, n , k):
+    cnt = 0
+    for i in range(n):
+        for j in range(i, n):
+            if arr[j] > arr[j+1]:
+                cnt += 1
+                if cnt != k:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                else:
+                    print(f"{arr[j]} {arr[j+1]}")
+                    break
+    if cnt < k:
+        print(-1)           
+    
+input = sys.stdin.readline
+n, k = map(int, input().rstrip().split()) # 배열 크기, 교환 횟수
+arr = list(map(int, input().rstrip().split())) # 배열 입력 받기           
+bubble_sort(arr, n, k)      
