77646dfe900c23324e4778737d13a5b4f44c5d10
Eunyu <82995817+ChunEunyu@users.noreply.github.com>
2022-08-25 19:19:55 +0900

    Update 15904.py

--- 으뉴에게 알려줄 문제/15904.py
+++ 으뉴에게 알려줄 문제/15904.py
@@ -1,14 +1,16 @@
 import sys
+
 input=sys.stdin.readline
-s=list(input())
+s=list(input()) # 리스트로 입력 받기
 st=0
+
 for i in "UCPC":
-    try:
-        st+=s[st:].index(i)
+    try: # s 리스트에 i 문자가 있는 인덱스 위치를 st 변수에 저장
+        st+=s[st:].index(i) 
     except:
         st=0
         break
-if st==0:
+if st==0: # ucpc가 s 리스트에 없는 경우
     print("I hate UCPC")
 else:
     print("I love UCPC")
