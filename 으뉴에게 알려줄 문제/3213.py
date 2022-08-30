aefbb88caad76659766ed11fe781d7c3c582283d
Eunyu <82995817+ChunEunyu@users.noreply.github.com>
2022-08-30 10:35:08 +0900

    Update 3213.py

--- 으뉴에게 알려줄 문제/3213.py
+++ 으뉴에게 알려줄 문제/3213.py
@@ -1,6 +1,8 @@
 import sys,math
 input=sys.stdin.readline
 s,t,f,f2=0,0,0,0
+
+#입력 받고 f, f2, t 변수에 카운트
 for _ in range(int(input())):
     a=input().rstrip()
     if a=="1/4":
@@ -9,9 +11,10 @@ for _ in range(int(input())):
         f2+=1
     elif a=="1/2":
         t+=1
-s+=t//2
-t%=2
-if f2>f:
+s+=t//2 # 1/2 개수를 2로 나눈 몫을 s에 저장
+t %= 2 # 나머지를 t에 저장
+
+if f2>f: 
     s+=f
     f2-=f
     s+=f2
